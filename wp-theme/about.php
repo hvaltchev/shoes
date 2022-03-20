81d230968f6f02e00963b328d1957da58d30f75a
Mike <mwatfordjr@gmail.com>
2022-03-20 14:59:12 -0700

    starting wp theme files

---
+++ wp-theme/about.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+
+<?php get_header(); ?>
+
+    <!-- HEADER -->
+    <header data-jarallax data-speed=".8" style="background-image: url(img/blog-holder.jpg);">
+        <div class="pt-10 pb-8 pt-md-15 pb-md-13 bg-black-50">
+            <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-8 col-lg-6 text-center">
+                <h1 class="display-6 fw-bold text-white">About Me</h1>
+                </div>
+            </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="py-7 py-md-9">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-8 col-lg-6 text-center">
+
+            <!-- Heading -->
+            <h2 class="mb-2">
+                SODA &amp; COOKIES
+            </h2>
+
+            <!-- Subheading -->
+            <p class="mb-6">
+            This is about creative nostalgia. Going to the corner store as a kid this was my staple. My lunch money would never be used for actual food. Instead it would be used to buy a sprite and whatever the option of grandma's cookies that were available. I would find a spot alone to draw, jot down ideas and just have my head in the clouds..
+
+As an adult, holding that mentality can be difficult. A way to catch my inspiration and get into a creative flow again was to give myself this site. A medium for my creative outlet and acting design journal. Hoping to bring back a spark of those carefree days.
+            </p>
+
+          </div>
+        </div>
+        <div class="row align-items-center justify-content-between">
+          <div class="col-md-6 order-md-2">
+
+            <!-- Image -->
+            <img class="img-fluid mw-md-120 clip-top-start" src="<?php bloginfo('template_directory'); ?>/img/blog-holder.jpg" alt="...">
+
+          </div>
+          <div class="col-md-5 order-md-1">
+
+            <!-- Text -->
+            <p class="lead text-black">
+              Mike Watford
+            </p>
+
+            <!-- Text -->
+            <p class="mb-6">
+            Pixel pusher from the east coast turned developer in the west coast. For five years I did freelance photography ran my own apparel company before making my way to silicon valley.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="mt-6 mt-md-9 mb-0 divider"></div>
+      </div>
+    </section>
+
+<?php endwhile; ?>
+<?php else : ?>
+
+<?php get_template_part( 'inc/post-none' ); ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
---
+++ wp-theme/footer.php
@@ -0,0 +1,79 @@
+<!-- FOOTER -->
+<footer class="py-7 py-md-9">
+      <div class="container-fluid px-4">
+        <div class="row gx-7">
+          
+          <div class="col-lg-2 col-sm-3">
+            <img src="<?php bloginfo('template_url'); ?>/img/footer-seal.png">
+          </div>
+          
+          <div class="col-sm-2 col-md-2">
+            <ul class="list-unstyled mb-6">
+              <li class="d-flex mb-2"><a href="/blog/">Blog</a></li>
+              <li class="d-flex mb-2"><a href="/shop/">Shop</a></li>
+              <li class="d-flex mb-2"><a href="/portfolio/">Portfolio</a></li>
+              <li class="d-flex mb-2"><a href="/about/">About</a></li>
+            </ul>
+          </div>
+
+          <div class="col-sm-2 col-md-2">
+            <ul class="list-unstyled mb-6">
+              <li class="d-flex mb-2"><a href="/contact/">Contact</a></li>
+              <li class="d-flex mb-2"><a href="/sessions/">Book a Session</a></li>
+              <li class="d-flex mb-2"><a href="https://www.instagram.com/bodegastaples/">Instagram</a></li>
+              <li class="d-flex mb-2"><a href="https://twitter.com/bodegastaples">Twitter</a></li>
+            </ul>
+          </div>
+          
+          <div class="col-sm-5 col-md-5">
+            <h3>Don't Miss out</h3>
+            <p>Stay in the know on creative indevours, new drops, and more.</p>
+            <!-- Begin Mailchimp Signup Form -->
+              <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7_dtp.css" rel="stylesheet" type="text/css">
+              <style type="text/css">
+                #mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+              </style>
+              <div id="mc_embed_signup">
+              <form action="https://sodaandcookies.us14.list-manage.com/subscribe/post?u=8b5c7f7858670686d46ce8d08&amp;id=0f0eaa123a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll" class="row footer-newsletter">
+                    <div class="col">
+                      <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required="" autocapitalize="off" autocorrect="off" type="email">
+                    </div>
+                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_8b5c7f7858670686d46ce8d08_0f0eaa123a" tabindex="-1" value="">
+                  </div>
+                    <div class="col">
+                        <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                          <img src="<?php bloginfo('template_url'); ?>/ img/submit-arrow.png" height="50" width="50">
+                        </button>
+                    </div>
+                      </div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+
+    <div class="container-fluid bg-black">
+      <div class="row legal-footer">
+        <div class="col-6 copyright">
+          &copy <?php echo date('Y'); ?> SODA &AMP; COOKIES. All Rights Reserved.
+        </div>
+        <div class="col-6 disclosure">
+          <a href="/disclosure-policy/">Privacy &amp; Disclosure Policy</a>
+        </div>
+      </div>
+    </div>
+    <!-- JAVASCRIPT -->
+    <!-- Vendor JS -->
+    <script src="<?php bloginfo('template_url'); ?>/js/vendor.bundle.js"></script>
+    
+    <!-- Theme JS -->
+    <script src="<?php bloginfo('template_url'); ?>/js/theme.bundle.js"></script>
+
+  </body>
+</html>
